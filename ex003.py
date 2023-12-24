71ef2109c9c146e18799e92d790cc7637b82c437
wilian cristian <crisf4f4@yahoo.com.br>
2023-12-24 00:49:24 -0300

    Initial commit

---
+++ ex003.py
@@ -0,0 +1,5 @@
+# crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input('digite um número  '))
+n2 = int(input('digite um número  '))
+soma = (n1+n2)
+print('A soma entre eles é igual a:{}'.format(soma))
